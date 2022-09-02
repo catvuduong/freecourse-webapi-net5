f8670e4c658bc7084d1974d0d396bb8a864928c7
vuduong-vizion <vu.duong@vizion.space>
2022-09-02 22:52:24 +0700

    initial good api.

---
+++ my-web-api-app/my-web-api-app/Controllers/GoodController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using my_web_api_app.Models;
+
+namespace my_web_api_app.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoodController : ControllerBase
+    {
+        public static List<Good> goods = new List<Good>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(goods);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                //LINQ [Object] Query
+                var good = goods.SingleOrDefault(good => good.GoodCode == System.Guid.Parse(id));
+                if (good == null)
+                {
+                    return NotFound();
+                }
+                return Ok(good);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(GoodVM goodVM)
+        {
+            var good = new Good
+            {
+                GoodCode = System.Guid.NewGuid(),
+                NameOfGood = goodVM.NameOfGood,
+                UnitPrice = goodVM.UnitPrice,
+            };
+            goods.Add(good);
+            return Ok(new
+            {
+                Success = true,
+                Data = good
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, Good goodEdit)
+        {
+            try
+            {
+                //LINQ [Object] Query
+                var good = goods.SingleOrDefault(good => good.GoodCode == System.Guid.Parse(id));
+                if (good == null)
+                {
+                    return NotFound();
+                }
+
+                if (id != good.GoodCode.ToString())
+                {
+                    return BadRequest();
+                }
+                //Update
+                good.NameOfGood = goodEdit.NameOfGood;
+                good.UnitPrice = goodEdit.UnitPrice;
+                return Ok(good);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                //LINQ [Object] Query
+                var good = goods.SingleOrDefault(good => good.GoodCode == System.Guid.Parse(id));
+                if (good == null)
+                {
+                    return NotFound();
+                }
+                //Update
+                goods.Remove(good);
+                return Ok(good);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ my-web-api-app/my-web-api-app/Models/Good.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace my_web_api_app.Models
+{
+    public class GoodVM
+    {
+        public string NameOfGood { get; set; }
+        public double UnitPrice
+        {
+            get; set;
+        }
+    }
+    public class Good : GoodVM
+    {
+        public Guid GoodCode { get; set; }
+    }
+}
